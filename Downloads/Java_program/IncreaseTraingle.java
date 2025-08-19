c963bf15c0f7c4075e6b8b752986b9709f11c30c
darshan <darshanmariyanna@gmail.com>
2025-08-19 14:38:18 +0530

    IncreaseTraingle

--- Downloads/Java_program/IncreaseTraingle.java
+++ Downloads/Java_program/IncreaseTraingle.java
@@ -1,15 +1,12 @@
-public class RightsideTraingle{
+public class IncreaseTraingle{
     public static void main(String[]args){
         int n=5;
         for(int i=1;i<=n;i++){
             for(int j=i;j<=n;j++){
-            System.out.print(" ");
+            System.out.print(" $");
             
         }
-         for(int j=1;j<=i;j++){
-            System.out.print(" *");
-            
-        }
+        
         System.out.println();
         }
     }
